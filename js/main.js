634014478de48a1c06fb6864141a3c2b76c40324
OverlordCrowley <maksim2004@bk.ru>
2021-07-14 20:47:43 +0600

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -19,3 +19,22 @@ const hotelSwiper = new Swiper('.hotel-slider', {
 
 });
 
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+});
+
